620afc56e1b786f41b8b17ca203388f3cd255e9c
Malcolm Bouzi & Awesome Ninja Fighter & suprfrye & Tracy Teague <pair+mbouzi.samguergen.suprfrye.tracyteague@devbootcamp.com>
2015-03-30 16:52:14 -0400

    Fix misspelling of survey. Add title and description to survey migration.

--- db/migrate/20150330153354_create_surveys.rb
+++ db/migrate/20150330153354_create_surveys.rb
@@ -1,6 +1,8 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
+      t.string :title, null: false
+      t.string :description, null: false
       t.belongs_to :creator, null: false
 
       t.timestamps null: false
