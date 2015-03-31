4e0fa33d1cf0cf1d7fe33080d6f8f86e222dbfc8
Awesome Ninja Fighter <samguergen@gmail.com>
2015-03-31 14:55:35 -0400

    added create form to surveys

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -3,7 +3,12 @@ get '/surveys' do
   erb :"survey/index"
 end
 
+get '/surveys/new' do
+  erb :"survey/new"
+end
+
 get '/surveys/:id' do
   @the_survey = Survey.find_by(:id => params[:id])
   erb :"survey/show"
 end
+
