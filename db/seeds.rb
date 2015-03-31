b97270b5707c3d55025c16eb05cf207e99a03990
Awesome Ninja Fighter <samguergen@gmail.com>
2015-03-31 09:48:04 -0400

    finished seeding choices

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ question1good = {:content => "Do you sometimes help people with their bags at th
 question2good = {:content => "Do you feel empathy for people?" ,:survey_id => survey_1.id }
 
 question1world = {:content => "How many countries have you been outside of your own?" ,:survey_id => survey_2.id }
-question2world = {:content => "Do you sometimes help people with their bags at the supermarket?" ,:survey_id => survey_2.id }
+question2world = {:content => "Can you speak more than one language?" ,:survey_id => survey_2.id }
 
 question1science = {:content => "How many bonds does oxygen have?" ,:survey_id => survey_3.id }
 question2science = {:content => "What is an organism?" ,:survey_id => survey_3.id }
@@ -53,4 +53,62 @@ question2_science = Question.create!(question2science)
 question1_stand = Question.create!(question1stand)
 question2_stand = Question.create!(question2stand)
 
+c1q1good = {:option => "Always" , :question_id => question1_good.id}
+c2q1good = {:option => "Sometimes" , :question_id => question1_good.id}
+c3q1good = {:option => "Never" , :question_id => question1_good.id}
 
+c1q2good = {:option => "Always" , :question_id => question2_good.id}
+c2q2good = {:option => "Sometimes" , :question_id => question2_good.id}
+c3q2good = {:option => "Never" , :question_id => question2_good.id}
+
+c1q1world = {:option => "At least 4" , :question_id => question1_world.id}
+c2q1world = {:option => "At least one" , :question_id => question1_world.id}
+c3q1world = {:option => "None" , :question_id => question1_world.id}
+
+c1q2world = {:option => "Yes, at least one fluently" , :question_id => question2_world.id}
+c2q2world = {:option => "Yes, one or two at basic, conversational level" , :question_id => question2_world.id}
+c3q2world = {:option => "No" , :question_id => question2_world.id}
+
+c1q1science = {:option => "3" , :question_id => question1_science.id}
+c2q1science = {:option => "2" , :question_id => question1_science.id}
+c3q1science = {:option => "1" , :question_id => question1_science.id}
+
+c1q2science = {:option => "A group of organs working together as a living entity" , :question_id => question2_science.id}
+c2q2science = {:option => "A human" , :question_id => question2_science.id}
+c3q3science = {:option => "Something that isn't dead" , :question_id => question2_science.id}
+
+c1q1stand = {:option => "Absolutely" , :question_id => question1_standid}
+c2q1stand = {:option => "Maybe" , :question_id => question1_stand.id}
+c3q1stand = {:option => "Never" , :question_id => question1_stand.id}
+
+c1q2stand = {:option => "Absolutely" , :question_id => question2_standid}
+c2q2stand = {:option => "Maybe" , :question_id => question2_stand.id}
+c3q2stand = {:option => "Never" , :question_id => question2_stand.id}
+
+c1q1_good = Choice.create!(c1q1good)
+c2q1_good = Choice.create!(c2q1good)
+c3q1_good = Choice.create!(c3q1good)
+c1q2_good = Choice.create!(c1q2good)
+c2q2_good = Choice.create!(c2q2good)
+c3q2_good = Choice.create!(c3q2good)
+
+c1q1_world = Choice.create!(c1q1world)
+c2q1_world = Choice.create!(c2q1world)
+c3q1_world = Choice.create!(c3q1world)
+c1q2_world  = Choice.create!(c1q2world)
+c2q2_world  = Choice.create!(c2q2world)
+c3q2_world  = Choice.create!(c3q2world)
+
+c1q1_science = Choice.create!(c1q1science)
+c2q1_science = Choice.create!(c2q1science)
+c3q1_science = Choice.create!(c3q1science)
+c1q2_science  = Choice.create!(c1q2science)
+c2q2_science  = Choice.create!(c2q2science)
+c3q2_science  = Choice.create!(c3q2science)
+
+c1q1_stand = Choice.create!(c1q1stand)
+c2q1_stand = Choice.create!(c2q1stand)
+c3q1_stand = Choice.create!(c3q1stand)
+c1q2_stand  = Choice.create!(c1q2stand)
+c2q2_stand  = Choice.create!(c2q2stand)
+c3q2_stand  = Choice.create!(c3q2stand)
